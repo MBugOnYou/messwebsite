2c954cdd2e8fea572b0c3effd7b1883216cb270e
mrmamunurrahman <mamunlink2011@gmail.com>
2020-08-07 21:44:02 +0600

    Updates

---
+++ precache-manifest.816c0dccba17cdbde74fe6953cf90154.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc17c8f9032a5860f29ca0dba3f6d9d",
+    "url": "/messwebsite/index.html"
+  },
+  {
+    "revision": "2d7f02515cd20d924144",
+    "url": "/messwebsite/static/css/2.9f8d8b69.chunk.css"
+  },
+  {
+    "revision": "c18805e6b226aeaacbef",
+    "url": "/messwebsite/static/css/main.903e2afe.chunk.css"
+  },
+  {
+    "revision": "2d7f02515cd20d924144",
+    "url": "/messwebsite/static/js/2.91a9181d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/messwebsite/static/js/2.91a9181d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18805e6b226aeaacbef",
+    "url": "/messwebsite/static/js/main.a1f334a2.chunk.js"
+  },
+  {
+    "revision": "463b737f526bdcc9256e",
+    "url": "/messwebsite/static/js/runtime-main.85c5eb13.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/messwebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/messwebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/messwebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/messwebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/messwebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/messwebsite/static/media/loading.9064981b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/messwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/components/Common/GetMonthName.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const GetMonthName = (number) => {
-
-    var montharray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    return  montharray[number - 1];
-};
-
-export default GetMonthName;
